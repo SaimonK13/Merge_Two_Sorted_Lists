685b70967412af900d817bf0117890d05050c27d
Saimon <47235269+SaimonK13@users.noreply.github.com>
2021-11-04 01:17:07 -0500

    Create Merge_Two_Sorted_Lists.java

---
+++ Merge_Two_Sorted_Lists.java
@@ -0,0 +1,48 @@
+/* Name: Merge Two Sorted Lists (LeetCode Problem #21)
+ * Description: merge two sorted linked lists and return it as a sorted list.
+ */
+ 
+class Merge_Two_Sorted_Lists {
+
+    /**
+    * Definition for singly-linked list.
+    * public class ListNode {
+    *     int val;
+    *     ListNode next;
+    *     ListNode() {}
+    *     ListNode(int val) { this.val = val; }
+    *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    * }
+    */
+    
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        
+        ListNode head = new ListNode(-1);
+        ListNode dummy = head;
+        
+        while(l1 != null && l2 != null) {
+            
+            if(l1.val < l2.val) {
+                head.next = l1;
+                l1 = l1.next;
+            }
+            
+            else {
+                head.next = l2;
+                l2 = l2.next;
+            }
+            
+            head = head.next;
+        }
+        
+        if(l1 != null){
+            head.next = l1;
+        }
+        
+        if(l2 != null) {
+            head.next = l2;
+        }
+        
+        return dummy.next;
+    }
+}
